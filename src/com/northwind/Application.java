f27469aa9ff95797e7115472a5b07d0c729b5568
Emrage <emre_yavus@live.nl>
2014-10-04 14:09:34 +0200

    Changed name of npc
    df

--- src/com/northwind/Application.java
+++ src/com/northwind/Application.java
@@ -1,12 +1,12 @@
 package com.northwind;
 
-import com.northwind.entity.npc.NPC;
+import com.northwind.entity.npc.Npc;
 import com.northwind.map.Position;
 
 public class Application {
 	
 	public static void main(String[] args) {
-		NPC bob = new NPC(new Position(0, 0), "Bob", "x'D");
+		Npc bob = new Npc(new Position(0, 0), "Bob", "x'D");
 		bob.move(5, 5);
 		System.out.println(bob.getPosition().toString());
 	}

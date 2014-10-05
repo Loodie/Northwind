a934360b0aad8071340e10957b3107aad35ffc9e
Loodie <Ludwig.kebabrulle@hotmail.com>
2014-10-05 11:30:20 +0200

    Set up LWJGL

--- src/com/northwind/Application.java
+++ src/com/northwind/Application.java
@@ -1,8 +1,24 @@
 package com.northwind;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Application {
 	
 	public static void main(String[] args) {
+		
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.setTitle("Northwind");
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		while(!Display.isCloseRequested()) {
+			Display.update();
+		}
 		
 	}	
 }
